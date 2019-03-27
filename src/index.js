168a4fa898752f89accdd775ab7badd2ccaf2e4b
ErickSkrauch <erickskrauch@yandex.ru>
2019-03-28 00:18:18 +0200

    Update example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-const test1 = require('./test1.png!=!my-loader!');
+const test1 = require('!!./test1.png!=!my-loader!');
+const test2 = require('!./test2.png!=!my-loader!');
+// const test3 = require('./test3.png!=!my-loader!');
 
 /**
  * I'm expecting a link to the generated image.
  * The file will be located in the path assets/test1.png,
  * according to the loader rules for png.
  */
-console.log(test1);
+console.log(test1, test2);
+// console.log(test3);
